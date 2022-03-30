5a038db92d540342c325d535ab5be9db1e5faceb
Simon Thür <thuer.simon@hotmail.com>
2022-03-30 22:27:41 +0200

    use pin 16, infinite loop for testing

--- main.cc
+++ main.cc
@@ -13,24 +13,23 @@
 
 #include "adc_driver_hx711.h"
 
-#define DATA_PIN_0 11
-#define DATA_PIN_1 26
-#define DATA_PIN_2 18
-#define DATA_PIN_3 22
-#define DATA_PIN_4 29
-#define DATA_PIN_5 32
+#define DATA_PIN_0 16
 #define DCLK_PIN   23
 
 int main( int argc, char const* argv[] )
 {
-    Pins           adc_input_pins = { DATA_PIN_0, DATA_PIN_1, DATA_PIN_2, DATA_PIN_3,
-                            DATA_PIN_4, DATA_PIN_5, DATA_PIN_5 };
+    Pins           adc_input_pins = { DATA_PIN_0 };
     AdcDriverHx711 adc_driver( DCLK_PIN, adc_input_pins );
-    Measurement    meas = adc_driver.read();
-    std::cout << "Measurement:";
-    for ( auto& m : meas )
+    int            i = 0;
+    for ( ;; )
         {
-            std::cout << '\n' << m;
+            Measurement meas = adc_driver.read();
+            std::cout << "Measurement " << i++ << ':';
+            for ( auto& m : meas )
+                {
+                    std::cout << '\n' << m;
+                }
+            std::cout << std::endl;
         }
 
 
