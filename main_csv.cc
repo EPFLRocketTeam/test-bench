b04052170083fe86eef612f6e985bc3f96ffdcc1
Simon Thür <thuer.simon@hotmail.com>
2022-04-16 21:16:27 +0200

    create csv cli

---
+++ main_csv.cc
@@ -0,0 +1,304 @@
+/**
+ * @file main_csv.cc
+ * @author Simon Thür (simon.thur@epfl.ch)
+ * @brief Program to create csv of measurements.
+ * @version 0.1
+ * @date 2022-04-12
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "adc_driver_hx711.h"
+
+
+#define DATA_PIN_0 11
+#define DATA_PIN_1 26
+#define DATA_PIN_2 18
+#define DATA_PIN_3 22
+#define DATA_PIN_4 29
+#define DATA_PIN_5 32
+
+#define DCLK_PIN 23
+
+
+void read( AdcDriverHx711& drvr, const std::string& file_name,
+           int nbr_of_measurements );
+void update_file( std::string& file, unsigned& meas_nbr );
+void edit_config( AdcDriverHx711& drvr, unsigned& measurements_per_reading );
+void print_config( AdcDriverHx711& drvr, unsigned& measurements_per_reading );
+void zero_adc( AdcDriverHx711& drvr );
+void print_help();
+
+
+int main( int argc, char const* argv[] )
+{
+    unsigned    meas_nbr        = 0;
+    unsigned    nbr_of_readings = 50;
+    std::string base_file;
+    if ( argc > 1 )
+        base_file = argv[ 1 ];
+    else
+        base_file = "test_bench_measurement";
+    Pins adc_input_pins = { DATA_PIN_0, DATA_PIN_1, DATA_PIN_2,
+                            DATA_PIN_3, DATA_PIN_4, DATA_PIN_5 };
+
+    AdcDriverHx711 adc_driver( DCLK_PIN, adc_input_pins );
+    print_config( adc_driver, nbr_of_readings );
+    std::cout << "CSV base file name: " << base_file << std::endl;
+
+
+    char action;
+    for ( ;; )
+        {
+            std::cout << ">> ";
+            std::cin >> action;
+            switch ( action )
+                {
+                    case 'q' :
+                        return 0;
+                        break;
+                    case 'r' :
+                        read(
+                            adc_driver,
+                            base_file + std::to_string( meas_nbr++ ) + ".csv",
+                            nbr_of_readings );
+                        break;
+                    case 'f' :
+                        update_file( base_file, meas_nbr );
+                        break;
+                    case 'e' :
+                        edit_config( adc_driver, nbr_of_readings );
+                        break;
+                    case 'p' :
+
+                        print_config( adc_driver, nbr_of_readings );
+                        std::cout << "CSV base file name: " << base_file
+                                  << std::endl;
+                        break;
+                    case 'z' :
+                        zero_adc( adc_driver );
+                        break;
+                    case 'h' :
+                        print_help();
+                        break;
+                    default :
+                        std::cout << std::endl;
+                        break;
+                }
+        }
+    return 0;
+}
+void print_config( AdcDriverHx711& drvr, unsigned& measurements_per_reading )
+{
+    std::cout << "Current  configuration:\nPins:\n    Clock:\n        DCLK:  "
+              << drvr.dclk_pin() << "\n    Data:             Offset:";
+    for ( unsigned i = 0; i < drvr.pins().size(); i++ )
+        {
+            std::cout << "\n        PIN " << i << ": " << drvr.pins()[ i ]
+                      << "         " << drvr.offset()[ i ];
+        }
+    std::cout << "\nGain:\n    " << drvr.gain_mode()
+              << "\nMeasurements per reading:\n    "
+              << measurements_per_reading << std::endl;
+}
+
+
+void read( AdcDriverHx711& drvr, const std::string& file_name,
+           int nbr_of_measurements )
+{
+    std::ofstream file = std::ofstream( file_name );
+    std::cout << "Starting measurements" << std::endl;
+    for ( int i = 0; i < nbr_of_measurements; i++ )
+        {
+            std::cout << "Reading " << i << std::endl;
+            for ( auto m : drvr.read() )
+                {
+                    file << m << ',';
+                }
+            file << std::endl;
+        }
+    file.close();
+    std::cout << "Finished measurements" << std::endl;
+}
+void update_file( std::string& file, unsigned& meas_nbr )
+{
+    char        choice;
+    std::string new_file;
+    unsigned    new_meas_nbr;
+    std::cout << "Current filename: " << file << std::endl
+              << "Enter a new filename:\n>> ";
+    std::cin >> new_file;
+    std::cout << "Current enumeration: " << meas_nbr << std::endl
+              << "Would you like to reset to 0? y/n/c (yes, no, custom)\n>> ";
+    std::cin >> choice;
+    switch ( choice )
+        {
+            case 'y' :
+                new_meas_nbr = 0;
+                break;
+            case 'n' :
+                new_meas_nbr = meas_nbr;
+                break;
+            case 'c' :
+                std::cout << "Enter new enumeration number\n>> ";
+                std::cin >> new_meas_nbr;
+                break;
+            default :
+                std::cout << "Answer not recognized, assume \'n\'"
+                          << std::endl;
+                break;
+        }
+    std::cout << "New configuration:\nFilename: " << new_file
+              << "\nEnumeration: " << new_meas_nbr
+              << "\n\n Would you like to accept changes? y/n\n>> ";
+    std::cin >> choice;
+    switch ( choice )
+        {
+            case 'y' :
+                meas_nbr = new_meas_nbr;
+                file     = new_file;
+                std::cout << "Changes accepted" << std::endl;
+                break;
+            case 'n' :
+                std::cout << "Changes discarded" << std::endl;
+                break;
+            default :
+                std::cout << "Unknown argument, changes discarded"
+                          << std::endl;
+                break;
+        }
+}
+void edit_config( AdcDriverHx711& drvr, unsigned& measurements_per_reading )
+{
+    unsigned new_measurements_per_reading = 0;
+    int      new_clock                    = 0;
+    int      new_gain                     = 0;
+    Pins     add_pins                     = {};
+    Pins     rm_pins                      = {};
+    bool     valid                        = true;
+    std::cout << "Do you wish to change the number of measurements per "
+                 "reading? y/n\n>> ";
+    char choice;
+    std::cin >> choice;
+    if ( choice == 'y' )
+        {
+            std::cout << "Currently, each reading consists of "
+                      << measurements_per_reading
+                      << " measurements.\nPlease enter the new number of"
+                         "measurements:\n>> ";
+            std::cin >> new_measurements_per_reading;
+            std::cout << "Accept " << new_measurements_per_reading
+                      << " measurements per reading? y/n\n>> ";
+            std::cin >> choice;
+            if ( choice == 'y' )
+                {
+                    measurements_per_reading = new_measurements_per_reading;
+                    std::cout << "Changes accepted" << std::endl;
+                }
+            else
+                {
+                    std::cout << "Changes discarded";
+                }
+        }
+
+    std::cout << "Do you wish to edit the gain mode? y/n\n>> ";
+    std::cin >> choice;
+    if ( choice == 'y' )
+        {
+            std::cout << "Current gain mode is " << drvr.gain_mode()
+                      << ".\nAvailable modes are:\n"
+                         " * Gain:   Channel\n"
+                         " * 128     A( Default )\n"
+                         " * 64      A\n"
+                         " * 32      B ";
+            do
+                {
+                    "\nEnter gain mode\n>> ";
+                    std::cin >> new_gain;
+                    std::cout << "Do you want to accept gain mode " << new_gain
+                              << "? y/n\n>> ";
+                    std::cin >> choice;
+                    if ( choice = 'y' )
+                        {
+                            valid = drvr.set_gain_mode( new_gain );
+                            if ( ! valid )
+                                std::cout << new_gain
+                                          << " was not a valid argument"
+                                          << std::endl;
+                            else
+                                std::cout << "Gain mode updated" << std::endl;
+                        }
+                    else
+                        {
+                            std::cout << "Input discarded" << std::endl;
+                            valid = true;
+                        }
+                }
+            while ( ! valid );
+        }
+
+
+    std::cout << "Do you wish to change the clock pin? y/n\n>> ";
+    std::cin >> choice;
+    if ( choice == 'y' )
+        {
+            std::cout << "Current clock pin is " << drvr.dclk_pin()
+                      << std::endl;
+            do
+                {
+                    std::cout << "Enter new clock pin\n>> ";
+                    std::cin >> new_clock;
+                    std::cout << "Do you wish to accept pin " << new_clock
+                              << " as clock pin? y/n\n>> ";
+                    std::cin >> choice;
+                    if ( choice == 'y' )
+                        {
+                            valid = drvr.set_dclk( new_clock );
+                            if ( ! valid )
+                                {
+                                    std::cout << "Something went wrong, "
+                                                 "attempt to force? y/n";
+                                    std::cin >> choice;
+                                    if ( choice == 'y' )
+                                        drvr.set_dclk( new_clock, true );
+                                }
+                            if ( valid )
+                                std::cout << "Clock was updated" << std::endl;
+                            else
+                                std::cout << "Clock was not updated"
+                                          << std::endl;
+                        }
+                    else
+                        {
+                            valid = true;
+                            std::cout << "Change discarded" << std::endl;
+                        }
+                }
+            while ( ! valid );
+        }
+}
+void zero_adc( AdcDriverHx711& drvr )
+{
+    std::cout << "Do you wish to recalibrate the driver? y/n\n>> ";
+    char choice;
+    std::cin >> choice;
+    switch ( choice )
+        {
+            case 'y' :
+                drvr.read_zero();
+                std::cout << "Driver calibrated to zero." << std::endl;
+                break;
+            case 'n' :
+                std::cout << "Request abandoned" << std::endl;
+            default :
+                std::cout << "Unknown command, request abandoned" << std::endl;
+                break;
+        }
+}
+void print_help() {}
