633b47d93cc3c94987fb1d2d3bebdde90bbc219a
AlbericLaj <albericlajarte@gmail.com>
2022-05-09 21:24:31 +0200

    corrected one pin and printing multiple measurement

--- main.cc
+++ main.cc
@@ -10,11 +10,13 @@
  */
 
 #include <iostream>
+#include <chrono>
+using namespace std::chrono;
 
 #include "adc_driver_hx711.h"
 
-#define DATA_PIN_0 11
-#define DATA_PIN_1 26
+#define DATA_PIN_0 16
+#define DATA_PIN_1 11
 #define DATA_PIN_2 18
 #define DATA_PIN_3 22
 #define DATA_PIN_4 29
@@ -25,16 +27,24 @@
 int main( int argc, char const* argv[] )
 {
     Pins adc_input_pins = { DATA_PIN_0, DATA_PIN_1, DATA_PIN_2,
-                            DATA_PIN_3, DATA_PIN_4, DATA_PIN_5 };
+                            DATA_PIN_3, DATA_PIN_4, DATA_PIN_5};
 
     AdcDriverHx711 adc_driver( DCLK_PIN, adc_input_pins );
+    auto start = high_resolution_clock::now();
+
+    while (1){
+
     Measurement    meas = adc_driver.read();
     std::cout << "Measurement:";
+    
     for ( auto& m : meas )
         {
-            std::cout << '\n' << m;
+            std::cout << ' ' << m;
         }
-
+auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    std::cout << " | Time: " << duration.count() << std::endl;
+    }
 
     return 0;
 }
